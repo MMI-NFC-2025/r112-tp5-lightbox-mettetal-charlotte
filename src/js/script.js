8980d9932e50e175d7817f2af1049eed36b3db14
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2025-12-03 09:57:25 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightBox");
+
+const lightboxImg= document.querySelector("#lightBox img");
+
+lightbox.querySelector("button")?.addEventListener("click", () => lightbox.close());
+
+document.body.addEventListener("click", (evt) => {
+    if (!evt.target.matches("[data-full-img]")) return;
+    document.querySelector('#lightBox img').src = evt.target.dataset.fullImg;
+    document.querySelector('#lightBox').showModal();
+});
